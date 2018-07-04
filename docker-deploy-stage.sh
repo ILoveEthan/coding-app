42a2d8e2d3f8496bda7d7c13051e1fee16803497
Zhenwei <iamaprotoss@gmail.com>
2018-07-03 22:00:32 -0400

    automate service update

--- docker-deploy-stage.sh
+++ docker-deploy-stage.sh
@@ -24,20 +24,33 @@ then
       fi
     }
 
+    update_service() {
+      if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service."
+        return 1
+      fi
+    }
+
     deploy_cluster() {
+      cluster="coding-app-staging-cluster"
+
       # users
+      service="coding-app-users-stage-service"
       template="ecs_users_stage_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_ACCOUNT_ID)
       echo "$task_def"
       register_definition
+      update_service
 
       # client
+      service="coding-app-client-staging-cluster"
       template="ecs_client_stage_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "$task_def"
       register_definition
+      update_service
 
     }
 
